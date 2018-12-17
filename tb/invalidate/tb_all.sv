fa7cb0bfa87e6bca2c0ac1c92b1ad4b6beef9ab4
Koen Wolters <kwolters@student.ethz.ch>
2018-12-17 01:48:44 +0100

    fix bug in invalidate all testbench that accidentially ignored l2

--- tb/invalidate/tb_all.sv
+++ tb/invalidate/tb_all.sv
@@ -70,7 +70,7 @@ module tb_all;
     localparam integer EN_L2TLB_ARRAY[`RAB_N_PORTS-1:0] = `EN_L2TLB_ARRAY;
     localparam N_SLICES_TOT = N_SLICES_ARRAY[0] + N_SLICES_ARRAY[1];
 
-    int address, max_va, va, pa;
+    int address, max_va, l1_va, va, pa;
 
     // NOTE: testbench currently assumes N_PORTS == 2 and L2 only on port 1 enabled
     assert(`RAB_N_PORTS == 2 && EN_L2TLB_ARRAY[0] == 0 && EN_L2TLB_ARRAY[1] == 1);
@@ -79,8 +79,19 @@ module tb_all;
     @(posedge start);
 
     $display("Inserting entries...");
-    // write L1 configuration (both host -> accelerator and accelerator -> host)
     va = 32'h00;
+    // write L2 configuration (only accelerator -> host)
+    for(int i=0; i<1 /*`RAB_L2_N_SET_ENTRIES*/; ++i) begin
+      for(int j=0; j<`RAB_L2_N_SETS; ++j) begin
+        address = 32'h8000 + j*4*`RAB_L2_N_SET_ENTRIES + i*4;
+        pa      = 32'hff000000 + va;
+        axi_config_drv.write_ok(address, (va >> 8) | 32'h7);
+        axi_config_drv.write_ok(address + 32'h1000, (pa >> 12));
+        va     += `PAGE_SIZE;
+      end
+    end
+    l1_va = va;
+    // write L1 configuration (both host -> accelerator and accelerator -> host)
     for(int i=0; i<N_SLICES_TOT; ++i) begin
       address  = 8'h20 + i*8'h20;
       pa       = 32'hff000000 + va;
@@ -90,19 +101,11 @@ module tb_all;
       axi_config_drv.write_ok(address + 32'h18, 3'b111);
       va      += `PAGE_SIZE;
     end
-
-    // write L2 configuration (only accelerator -> host)
-    for(int i=0; i<`RAB_L2_N_SETS*`RAB_L2_N_SET_ENTRIES; ++i) begin
-      address = 32'h8000 + i*8'h04;
-      pa      = 32'hff000000 + va;
-      axi_config_drv.write_ok(address, va);
-      axi_config_drv.write_ok(address, pa);
-      va     += `PAGE_SIZE;
-    end
+    max_va = va;
 
     $display("Checking if entries correctly setup...");
     // check L1 configuration
-    va = 32'h00;
+    va = l1_va;
     for(int i=0; i<N_SLICES_TOT; ++i) begin
       address = 8'h20 + i*8'h20;
       pa      = 32'hff000000 + va;
@@ -112,22 +115,24 @@ module tb_all;
       axi_config_drv.read_exp(address + 32'h18, 3'b111);
       va     += `PAGE_SIZE;
     end
-    max_va = va;
 
     // check all addresses if they are hit as expected
-    for(int addr = 0; addr<max_va; addr += `PAGE_SIZE) begin
+    for(int addr=0; addr<max_va; addr+=`PAGE_SIZE) begin
       ax_beat.ax_addr = addr;
       ax_beat.ax_size = 3'b101;
       axi_master_drv.send_ar(ax_beat);
-      r_beat.r_last = 1'b0;
       axi_master_drv.recv_r(r_beat);
-      if(addr/`PAGE_SIZE < N_SLICES_ARRAY[0] && r_beat.r_resp != 2'b10) begin
-        // first addresses should fail as these are host slices
-        $error("Expected 0x%08x not to hit as it mapped in the host slices", addr);
-      end else if(addr/`PAGE_SIZE > N_SLICES_ARRAY[0] && r_beat.r_resp != 2'b00) begin
-        $error("Expected 0x%08x to hit", addr);
-      end else if(addr/`PAGE_SIZE > N_SLICES_ARRAY[0] && r_beat.r_data != 32'hff000000 + addr) begin
-        $error("Hit for 0x%08x expected at 0x%08x instead of 0x%08x", addr, 32'hff000000 + addr, r_beat.r_data);
+      if(addr >= l1_va && (addr - l1_va)/`PAGE_SIZE < N_SLICES_ARRAY[0]) begin
+        if(r_beat.r_resp != 2'b10) begin
+          // these addresses should fail as these are host slices
+          $error("Expected 0x%08x not to hit as it mapped in the host slices", addr);
+        end
+      end else begin
+        if(r_beat.r_resp != 2'b00) begin
+          $error("Expected 0x%08x to hit", addr);
+        end else if(r_beat.r_data != 32'hff000000 + addr) begin
+          $error("Hit for 0x%08x expected at 0x%08x instead of 0x%08x", addr, 32'hff000000 + addr, r_beat.r_data);
+        end
       end
     end
 
@@ -142,7 +147,7 @@ module tb_all;
 
     $display("Verifying disabled l1/l2 config...");
     // check if L1 configuration fails (both host -> accelerator and accelerator -> host)
-    va = 32'h00;
+    va = l1_va;
     for(int i=0; i<N_SLICES_TOT; ++i) begin
       address  = 8'h20 + i*8'h20;
       pa       = 32'hff000000 + va;
@@ -154,17 +159,15 @@ module tb_all;
     end
 
     // check if L2 configuration failed (only accelerator -> host)
-    for(int i=0; i<`RAB_L2_N_SETS*`RAB_L2_N_SET_ENTRIES; ++i) begin
+    for(int i=0; i<`RAB_L2_N_SET_ENTRIES; ++i) begin
       address = 32'h8000 + i*8'h04;
-      pa      = 32'hff000000 + va;
-      axi_config_drv.write_err(address, va);
-      axi_config_drv.write_err(address, pa);
-      va     += `PAGE_SIZE;
+      axi_config_drv.write_err(address, 32'h00);
+      axi_config_drv.write_err(address + 32'h1000, 32'h00);
     end
 
     $display("Checking if entries invalidated...");
     // check L1 configuration
-    va = 32'h00;
+    va = l1_va;
     for(int i=0; i<N_SLICES_TOT; ++i) begin
       address = 8'h20 + i*8'h20;
       pa      = 32'hff000000 + va;
@@ -176,14 +179,13 @@ module tb_all;
     end
 
     // check all addresses if they miss as expected
-    for(int addr = 0; addr<max_va; addr += `PAGE_SIZE) begin
+    for(int addr=0; addr<max_va; addr+=`PAGE_SIZE) begin
       ax_beat.ax_addr = addr;
       ax_beat.ax_size = 3'b101;
       axi_master_drv.send_ar(ax_beat);
-      r_beat.r_last = 1'b0;
       axi_master_drv.recv_r(r_beat);
       if(r_beat.r_resp != 2'b10) begin
-        $error("Expected 0x%08x not to hit as all entries have been invalidated", addr);
+        $error("Expected 0x%08x to miss as all entries have been invalidated", addr);
       end
     end
 
